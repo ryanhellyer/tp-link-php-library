159f6d2a3f0f38e31387b8fe1bbb4a441bfed321
Ryan <ryanhellyer@gmail.com>
2020-10-03 00:11:23 +0200

    too lazy to write commit message

--- index.php
+++ index.php
@@ -5,19 +5,27 @@
  */
 class TP_Link_API {
 
+	private $username;
+	private $password;
+
 	/**
 	 * Class constructor.
 	 */
 	public function __construct( $username, $password ) {
+		$this->username = $username;
+		$this->password = $password;
+	}
 
-		$token   = $this->get_token( $username, $password );
-
-		$devices = $this->get_devices( $token );
+	/**
+	 * Toggle lights.
+	 */
+	public function toggle() {
+		$devices = $this->get_devices();
 
 		foreach ( $devices as $device ) {
 			$device_id = $device['deviceId'];
-			$this->turn_device_on( $device_id, $token );
-			$this->turn_device_off( $device_id, $token );
+			$this->turn_device_on( $device_id );
+			$this->turn_device_off( $device_id );
 		}
 	}
 
@@ -26,10 +34,10 @@ class TP_Link_API {
 	 *
 	 * @access private
 	 * @param int $device_id The device ID.
-	 * @param string $token Authentication token.
 	 * @return bool True if successful.
 	 */
-	private function turn_device_off( $device_id, $token ) {
+	private function turn_device_off( $device_id ) {
+		$token = $this->get_token();
 
 		$command = 'curl -XPOST
 			-H "Content-type: application/json"
@@ -53,10 +61,10 @@ class TP_Link_API {
 	 *
 	 * @access private
 	 * @param int $device_id The device ID.
-	 * @param string $token Authentication token.
 	 * @return bool True if successful.
 	 */
-	private function turn_device_on( $device_id, $token ) {
+	private function turn_device_on( $device_id ) {
+		$token = $this->get_token();
 
 		$command = 'curl -XPOST
 			-H "Content-type: application/json"
@@ -79,13 +87,18 @@ class TP_Link_API {
 	 * Get the devices.
 	 *
 	 * @access private
-	 * @param string $token Authentication token.
 	 * @return array|false False if not successful, else array of devices.
 	 */
-	private function get_devices( $token ) {
+	private function get_devices() {
+		$token = $this->get_token();
 
 		// Return cache if available.
 		if ( $this->get_cache( 'devices', 60 ) ) {
+$bla = $this->get_cache( 'devices', 60 );
+echo "\n\n\n\n";
+print_r( $bla );
+die;
+
 			return $this->get_cache( 'devices', 60 );
 		}
 
@@ -112,11 +125,9 @@ class TP_Link_API {
 	 * Get the authentication token.
 	 *
 	 * @access private
-	 * @param string $username The username.
-	 * @param string $password The password.
 	 * @return array|false False if not successful, else array of devices.
 	 */
-	public function get_token( $username, $password ) {
+	public function get_token() {
 
 		// Return cache if available.
 		if ( $this->get_cache( 'token', 60 ) ) {
@@ -130,20 +141,20 @@ class TP_Link_API {
 			\"url\":\"https://wap.tplinkcloud.com\",
 			\"params\":{
 			  \"appType\":\"Kasa_Android\",
-			  \"cloudPassword\":\"$password\",
-			  \"cloudUserName\":\"$username\",
+			  \"cloudPassword\":\"$this->password\",
+			  \"cloudUserName\":\"$this->username\",
 			  \"terminalUUID\":\"96a26069-5b27-40e5-8408-de7d2371bf16\"
 			}
 		  }'
 		  'https://wap.tplinkcloud.com/?appName=Kasa_Android&termID=96a26069-5b27-40e5-8408-de7d2371bf16&appVer=1.4.4.607&ospf=Android%2B6.0.1&netType=wifi&locale=es_ES'";
 		$command = $this->prepare_command( $command );
-//echo $command;die;
+
 		$response = shell_exec( $command );
-//print_r( $response );
 		$response = json_decode( $response, true );
 
 		if ( isset( $response['result']['token'] ) ) {
 			$token = $response['result']['token'];
+			$token = str_replace( '"', '', $token );
 
 			$this->update_cache( 'token', $token );
 
@@ -180,7 +191,7 @@ class TP_Link_API {
 	 */
 	private function get_cache( $key, $expiry ) {
 		$data = file_get_contents( '../' . $key . '.txt' );
-		$data = explode( ':', $data );
+		$data = explode( '|', $data );
 		if ( isset( $data[0] ) && isset( $data[1] ) ) {
 			$last_time = $data[0];
 			$result     = $data[1];
@@ -205,7 +216,12 @@ class TP_Link_API {
 	 * @param string $data The data to store.
 	 */
 	private function update_cache( $key, $data ) {
-		file_put_contents( '../' . $key . '.txt', time() . ':' . json_encode( $data ) );
+
+		if ( is_array( $data ) ) {
+			$data = json_encode( $data );
+		}
+
+		file_put_contents( '../' . $key . '.txt', time() . '|' . $data );
 	}
 
 }
